d8cc539116c3d4d201ba88bc078a4ba1322db714
Alex Fetea <alexfetea@alexs-mbp.mynetworksettings.com>
2023-10-02 21:55:46 -0400

    Added login/registration logic

--- ScavengerHuntApp/urls.py
+++ ScavengerHuntApp/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from mainapp.views import index
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),  # Use the index view here
+    path('accounts/', include('allauth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', TemplateView.as_view(template_name="register.html"), name='register'),
+
 ]
+
