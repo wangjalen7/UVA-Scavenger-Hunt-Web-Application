74c1a9df3f4695fe688139a4fb024183269f7545
olyall <owl8rg@virginia.edu>
2023-10-11 10:50:31 -0400

    created initial templates

--- ScavengerHuntApp/urls.py
+++ ScavengerHuntApp/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', TemplateView.as_view(template_name="register.html"), name='register'),
-
+    path('home/', TemplateView.as_view(template_name="home.html"), name='home'),
 ]
 
